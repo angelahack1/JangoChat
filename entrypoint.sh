0be171ad7aa2f37ee0d91d00dced0e573613affe
angelahack1 <angelakimichellle@hotmail.com>
2025-07-15 20:06:38 -0600

    Fixing static in django settings and entrypoint, by angelahack1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
 
 # Create superuser from environment variables
 echo "Creating superuser..."
